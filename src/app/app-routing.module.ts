a58f618f38ff49271f15e932131de0ff1e47ebef
ANDRES <solintegralesei@gmail.com>
2023-02-10 12:46:16 -0500

    2.0 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: PrincipalComponent,
+      children: [
+        { path: 'dashboard', component: DashboardComponent },
+        { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+      ]
+  },
+  { path: 'login', component: LoginComponent },
+  
+];
+
+//const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './estructura/header/header.component';
+import { NavComponent } from './estructura/nav/nav.component';
+import { FooterComponent } from './estructura/footer/footer.component';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    PrincipalComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
